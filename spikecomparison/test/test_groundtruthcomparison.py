167037796de49c299b7ccf29605804f4b53953ce
Samuel Garcia <sam.garcia.die@gmail.com>
2020-02-13 13:59:11 +0100

    oups

--- spikecomparison/test/test_groundtruthcomparison.py
+++ spikecomparison/test/test_groundtruthcomparison.py
@@ -16,7 +16,7 @@ def make_sorting(times1, labels1, times2, labels2):
 
 def test_compare_sorter_to_ground_truth():
     # simple match
-    gt_sorting, tested_sorting = make_sorting([100, 200, 300, 400, 500, 600], [0, 0, 1, 0, 1, 1],
+    gt_sorting, tested_sorting = make_sorting([100, 200, 300, 400, 500, 600, 700], [0, 0, 1, 0, 1, 1, 1],
                                               [101, 201, 301, 302, 401, 501, 502, 601, 900], [0, 0, 5, 6, 0, 5, 6, 5, 11])
 
     for match_mode in ('hungarian', 'best'):
@@ -26,7 +26,7 @@ def test_compare_sorter_to_ground_truth():
         sc = compare_sorter_to_ground_truth(gt_sorting, tested_sorting, exhaustive_gt=True,
                                             match_mode=match_mode, compute_labels=compute_labels)
 
-        assert_array_equal(sc.event_counts1.values, [3, 3])
+        assert_array_equal(sc.event_counts1.values, [3, 4])
         assert_array_equal(sc.event_counts2.values, [3, 3, 2, 1])
 
         assert_array_equal(sc.possible_match_12[1], [5, 6])
@@ -62,6 +62,7 @@ def test_compare_sorter_to_ground_truth():
 
     # test well detected units depending on thresholds
     good_units = sc.get_well_detected_units()  # tp_thresh=0.95 default value
+    print(good_units)
     assert_array_equal(good_units, [0, ])
     good_units = sc.get_well_detected_units(well_detected_score=0.95)
     assert_array_equal(good_units, [0, ])
